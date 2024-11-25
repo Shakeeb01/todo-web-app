4f2e0baf5eabc90dcbb4547306035b47b2a0dbda
Shakeeb ur Rehman <shakeeburrehman78@gmail.com>
2024-11-25 11:02:05 +0500

    Configure some settings related to core of the app

--- main/settings.py
+++ main/settings.py
@@ -40,7 +40,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'practice.urls'
+ROOT_URLCONF = 'main.urls'
 
 TEMPLATES = [
     {
@@ -58,7 +58,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'practice.wsgi.application'
+WSGI_APPLICATION = 'main.wsgi.application'
 
 
 # Database
